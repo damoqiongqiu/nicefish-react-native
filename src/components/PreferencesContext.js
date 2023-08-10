25b6b084fdcdc2b26b39741912a13e1960e9939c
大漠穷秋 <damoqiongqiu@126.com>
2023-08-10 20:53:24 +0800

    添加注释

--- src/components/PreferencesContext.js
+++ src/components/PreferencesContext.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * TODO:获取系统当前的模式，自动跟随系统。
+ * @author 大漠穷秋
+ * @date 2023-08-10 20:52:27
+ */
 export const PreferencesContext = React.createContext({
     toggleTheme: () => { },
     isThemeDark: false,
